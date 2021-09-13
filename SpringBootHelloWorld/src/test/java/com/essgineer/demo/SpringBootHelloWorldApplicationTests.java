504a7910a683e590a82be6b7d87480a489415719
Me <Me@192.168.6.238>
2021-09-13 16:31:58 +0200

    no comment

---
+++ SpringBootHelloWorld/src/test/java/com/essgineer/demo/SpringBootHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essgineer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
